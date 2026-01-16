e6c8c4b46aa9e3b562c3e7c5f8790de7dff5e98d
Linus Metzler <hi@linusmetzler.me>
2026-01-16 09:56:39 +0100

    bump after require

--- src/Checks/Checker.php
+++ src/Checks/Checker.php
@@ -43,6 +43,7 @@ class Checker
     public function bumpsComposer(): CheckResult
     {
         return $this->hasPostUpdateScript('composer bump')
+            && $this->hasPostRequireScript('composer bump')
             ? CheckResult::PASS
             : CheckResult::FAIL;
     }
@@ -887,6 +888,11 @@ class Checker
     private function hasPostUpdateScript(string $match): bool
     {
         return $this->checkComposerScript('post-update-cmd', $match);
+    }
+
+    private function hasPostRequireScript(string $match): bool
+    {
+        return $this->checkComposerScript('post-require-cmd', $match);
     }
 
     /**
